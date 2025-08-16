607d07672a402dc73cc489c965361610452096fd
MDU-student <ada24002@student.mdu.se>
2025-08-16 18:31:26 +0200

    style: changed to sizes and added disabled status to prevent 1 before 2

--- material-theme/lib/main.dart
+++ material-theme/lib/main.dart
@@ -88,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ),  
           ],
         ),
       ),
--- teams_modifier_app/lib/home_page.dart
+++ teams_modifier_app/lib/home_page.dart
@@ -19,22 +19,71 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final windowHeight = MediaQuery.of(context).size.height;
+    final windowWidth = MediaQuery.of(context).size.width;
+    final colorTheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    final btnTheme = Theme.of(context).buttonTheme;
+
+    bool isButtonEnabled = false;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('1. Upload a gif'),
-            ElevatedButton(onPressed: () {}, child: Text("Upload")),
-            const Text('2. Press "Set background"'),
-            ElevatedButton(onPressed: () {}, child: Text("Set background")),
-
-            const Text('3. Select the video background in teams!'),
-          ],
+        child: Container(
+          alignment: Alignment.topCenter,
+          color: Theme.of(context).colorScheme.surfaceContainerLow,
+          padding: EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('1. Upload a gif', style: textTheme.displaySmall),
+              SizedBox(height: 20),
+              Container(
+                width: 256,
+                height: 256,
+                color: colorTheme.surfaceContainer,
+                child: Text("placeholder for chosen gif"),
+              ),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: () {},
+                style: FilledButton.styleFrom(
+                  textStyle: textTheme.headlineLarge,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 20,
+                  ), // Increase padding
+                ),
+                child: Text("Upload"),
+              ),
+              SizedBox(height: 40),
+              Text('2. Press "Set background"', style: textTheme.displaySmall),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: isButtonEnabled
+                    ? () {
+                        // Button action
+                      }
+                    : null,
+                style: FilledButton.styleFrom(
+                  textStyle: textTheme.headlineLarge,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 20,
+                  ), // Increase padding
+                ),
+                child: Text("Set background"),
+              ),
+              SizedBox(height: 20),
+              Text(
+                '3. Select the video background in teams!',
+                style: textTheme.displaySmall,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
